ee1882e1a0b5d1cfb40c2bf68fc31e4702f707bf
Benoit TRAVERS <benoit.travers@zenika.com>
2019-02-06 14:10:45 +0100

    Basic project with command line runner

--- src/main/kotlin/org/breizhcamp/talk/springwebflux/SpringWebfluxApplication.kt
+++ src/main/kotlin/org/breizhcamp/talk/springwebflux/SpringWebfluxApplication.kt
@@ -1,10 +1,27 @@
 package org.breizhcamp.talk.springwebflux
 
+import org.breizhcamp.talk.springwebflux.repository.TweetRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class SpringWebfluxApplication
+class SpringWebfluxApplication {
+
+    @Bean
+    fun commandLineRunner(tweetRepository: TweetRepository): CommandLineRunner {
+        return CommandLineRunner {
+            tweetRepository.findAll()
+                    .map { tweet -> tweet.hashtag }
+                    .subscribe(::println)
+
+            tweetRepository.findById(33875)
+                    .subscribe(::println)
+        }
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringWebfluxApplication>(*args)
