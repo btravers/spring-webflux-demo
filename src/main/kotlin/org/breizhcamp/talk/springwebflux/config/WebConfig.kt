18bddb9f3fefd717d0ba215b4f21ba64c7cceb75
Alan <alan.menant@gmail.com>
2019-03-10 14:27:48 +0100

    Update init project

--- src/main/kotlin/org/breizhcamp/talk/springwebflux/config/WebConfig.kt
+++ src/main/kotlin/org/breizhcamp/talk/springwebflux/config/WebConfig.kt
@@ -1,12 +1,11 @@
 package org.breizhcamp.talk.springwebflux.config
 
 import org.breizhcamp.talk.springwebflux.serialization.OBJECT_MAPPER
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.server.WebFilter
-import org.springframework.web.server.WebFilterChain
-import reactor.core.publisher.Mono
 
 @Configuration
 class WebConfig {
@@ -16,14 +15,17 @@ class WebConfig {
 
     @Bean
     fun webFilter(): WebFilter {
-        return object: WebFilter {
-            override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-                exchange.response
-                        .headers
-                        .add("Cache-Control", "no-transform")
-                return chain.filter(exchange)
-            }
+        return WebFilter { exchange, chain ->
+            exchange.response
+                    .headers
+                    .add("Cache-Control", "no-transform")
+            chain.filter(exchange)
         }
+    }
+
+    @Bean
+    fun webClient(@Value("\${tweet-service.base-url}") tweetServiceBaseUrl: String): WebClient {
+        return WebClient.create(tweetServiceBaseUrl)
     }
 
 }
---
+++ src/main/kotlin/org/breizhcamp/talk/springwebflux/web/TweetController.kt
@@ -0,0 +1,2 @@
+package org.breizhcamp.talk.springwebflux.web
+
